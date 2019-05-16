e11a5a7098f9a9a3468ee354cac499ca70de8e04
1997alireza <torabian.alireza@gmail.com>
2019-05-16 21:55:13 +0430

    restructure

--- HW2/line_search_algorithms.py
+++ HW2/line_search_algorithms.py
@@ -1,48 +1,8 @@
 import numpy as np
+from HW2.tools import norm_p, \
+    rosenbrock_function as f, rosenbrock_grad as grad_f, rosenbrock_inverse_hessian as inv_hessian_f
 
 TOLERANCE, MAX_STEP = .001, 1000
-
-
-def f(x1, x2):
-    """
-
-    :param x1:
-    :param x2:
-    :return: Rosenbrock function
-    """
-    return 100 * (x2 - x1**2)**2 + (1 - x1)**2
-
-
-def grad_f(x1, x2):
-    """
-
-    :param x1:
-    :param x2:
-    :return: gradient of Rosenbrock function
-    """
-    g1 = -400 * (x2 - x1**2) * x1 + 2 * (x1 - 1)
-    g2 = 200 * (x2 - x1**2)
-
-    return np.array([g1, g2])
-
-
-def hessian_inverse_f(x1, x2):
-    """
-
-    :param x1:
-    :param x2:
-    :return: hessian inverse of Rosenbrock function
-    """
-    h_inverse_b = np.array([
-        [200, 400*x1],
-        [400*x1, 400*(3*x1**2-x2)+2]
-    ])
-    return h_inverse_b / (80000 * (3 * x1**2 - x2) + 400 - 160000 * x1**2)
-
-
-def norm_p(x, p=2):
-    assert p >= 1
-    return sum(x**p) ** (1/p)
 
 
 def steepest_descent_direction(x):
@@ -50,7 +10,7 @@ def steepest_descent_direction(x):
 
 
 def newton_direction(x):
-    h_i = hessian_inverse_f(*x)
+    h_i = inv_hessian_f(*x)
     g = grad_f(*x)
     return -1 * np.dot(h_i, g)
 
@@ -60,7 +20,7 @@ def BFGS_direction(x):
     global last_BFGS_H, last_step_length, last_x
 
     if last_BFGS_H is None:
-        H = hessian_inverse_f(*x)
+        H = inv_hessian_f(*x)
     else:
         last_p = -1 * np.dot(last_BFGS_H, grad_f(*x))
         x_kPlus1 = last_x
@@ -107,16 +67,7 @@ def line_search(x, descent_direction_algorithm=steepest_descent_direction):
         alpha = backtracking_line_length(x, p)
         x = x + alpha * p
         last_x, last_step_length = x, alpha
-        print('{}. new x: {} using alpha={}'.format(step, x, alpha))
         step += 1
-    print()
+        # print('{}. new x: {} using alpha={}'.format(step, x, alpha))
+    print('number of steps: ', step)
     return x
-
-
-if __name__ == '__main__':
-    in_x = [
-        np.array([1.2, 1.2]),
-        np.array([-1.2, 1])
-    ]
-    results = [line_search(xi, BFGS_direction) for xi in in_x]
-    print('Results :', results)
---
+++ HW2/tools.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+def norm_p(x, p=2):
+    assert p >= 1
+    return sum(x**p) ** (1/p)
+
+
+def rosenbrock_function(x1, x2):
+    """
+
+    :param x1:
+    :param x2:
+    :return: Rosenbrock function
+    """
+    return 100 * (x2 - x1**2)**2 + (1 - x1)**2
+
+
+def rosenbrock_grad(x1, x2):
+    """
+
+    :param x1:
+    :param x2:
+    :return: gradient of Rosenbrock function
+    """
+    g1 = -400 * (x2 - x1**2) * x1 + 2 * (x1 - 1)
+    g2 = 200 * (x2 - x1**2)
+
+    return np.array([g1, g2])
+
+
+def rosenbrock_hessian(x1, x2):
+    """
+
+    :param x1:
+    :param x2:
+    :return: hessian of Rosenbrock function
+    """
+    return np.array([
+        [400 * (3 * x1 ** 2 - x2) + 2, -400 * x1],
+        [-400 * x1, 200]
+    ])
+
+
+def rosenbrock_inverse_hessian(x1, x2):
+    """
+
+    :param x1:
+    :param x2:
+    :return: inverse hessian of Rosenbrock function
+    """
+    h = rosenbrock_hessian(x1, x2)
+    a = h[0][0]
+    b = h[0][1]
+    c = h[1][0]
+    d = h[1][1]
+    return np.array([[d, -b], [-c, a]]) / (a*d-b*c)
