ecb61454dc075aec22f70502c087f8ee707d2d2d
1997alireza <torabian.alireza@gmail.com>
2019-05-17 02:06:46 +0430

    forgot to delete prev functions :d

--- HW2/tools.py
+++ HW2/tools.py
@@ -26,38 +26,3 @@ class RosenbrockProvider:
             [400 * (3 * x1 ** 2 - x2) + 2, -400 * x1],
             [-400 * x1, 200]
         ])
-
-def rosenbrock_function(x1, x2):
-    """
-
-    :param x1:
-    :param x2:
-    :return: Rosenbrock function
-    """
-    return 100 * (x2 - x1**2)**2 + (1 - x1)**2
-
-
-def rosenbrock_grad(x1, x2):
-    """
-
-    :param x1:
-    :param x2:
-    :return: gradient of Rosenbrock function
-    """
-    g1 = -400 * (x2 - x1**2) * x1 + 2 * (x1 - 1)
-    g2 = 200 * (x2 - x1**2)
-
-    return np.array([g1, g2])
-
-
-def rosenbrock_hessian(x1, x2):
-    """
-
-    :param x1:
-    :param x2:
-    :return: hessian of Rosenbrock function
-    """
-    return np.array([
-        [400 * (3 * x1 ** 2 - x2) + 2, -400 * x1],
-        [-400 * x1, 200]
-    ])
