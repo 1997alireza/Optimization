da0954f76a95249df928028be0a8a64604d0d5de
1997alireza <torabian.alireza@gmail.com>
2019-05-17 21:38:25 +0430

    imporve

--- HW2/line_search_algorithms.py
+++ HW2/line_search_algorithms.py
@@ -1,14 +1,14 @@
 import numpy as np
-from HW2.tools import norm_p, RosenbrockProvider as F_Provider
+from HW2.tools import norm_p
 
 
 def steepest_descent_direction(x):
-    return -1 * F_Provider.grad(*x)
+    return -1 * f_provider.grad(*x)
 
 
 def newton_direction(x):
-    h_i = np.linalg.inv(F_Provider.hessian(*x))
-    g = F_Provider.grad(*x)
+    h_i = np.linalg.inv(f_provider.hessian(*x))
+    g = f_provider.grad(*x)
     return -1 * np.dot(h_i, g)
 
 
@@ -17,27 +17,28 @@ def BFGS_direction(x):
     global last_BFGS_H, last_step_length, last_x
 
     if last_BFGS_H is None:
-        H = np.linalg.inv(F_Provider.hessian(*x))
+        H = np.linalg.inv(f_provider.hessian(*x))
     else:
-        last_p = -1 * np.dot(last_BFGS_H, F_Provider.grad(*x))
+        last_p = -1 * np.dot(last_BFGS_H, f_provider.grad(*x))
         x_kPlus1 = last_x
         s = last_step_length * last_p
         x_k = x_kPlus1 - s
-        y = F_Provider.grad(*x_kPlus1) - F_Provider.grad(*x_k)
+        y = f_provider.grad(*x_kPlus1) - f_provider.grad(*x_k)
+        # print("-----------", s,"============", y)
         m = 1 / np.dot(s, y)
-        s = s[:, None]  # shape: (2,) -> (2,1)
+        s = s[:, None]  # shape: (size of x,) -> (size of x,1)
         y = y[:, None]
         s_yT = np.dot(s, y.T)
         y_sT = np.dot(y, s.T)
         s_sT = np.dot(s, s.T)
-        mL = np.identity(2) - m * s_yT
-        mR = np.identity(2) - m * y_sT
+        mL = np.identity(np.shape(x)[0]) - m * s_yT
+        mR = np.identity(np.shape(x)[0]) - m * y_sT
         rL = np.dot(np.dot(mL, last_BFGS_H), mR)
         rR = m * s_sT
         H = rL + rR
 
     last_BFGS_H = H
-    return -1 * np.dot(H, F_Provider.grad(*x))
+    return -1 * np.dot(H, f_provider.grad(*x))
 
 
 def first_wolf_condition(x, p, alpha, c):
@@ -45,22 +46,25 @@ def first_wolf_condition(x, p, alpha, c):
     sufficient decrease condition
     """
     new_x = x + alpha * p
-    return F_Provider.f(*new_x) <= F_Provider.f(*x) + c * alpha * np.dot(p, F_Provider.grad(*x))
+    return f_provider.f(*new_x) <= f_provider.f(*x) + c * alpha * np.dot(p, f_provider.grad(*x))
 
 
 def backtracking_line_length(x, p):
     alpha, m, c = 1, .5, .5  # parameters
     while not first_wolf_condition(x, p, alpha, c):
         alpha = alpha * m
+        if alpha < TOLERANCE:
+            return alpha
     return alpha
 
 
-TOLERANCE, MAX_STEP = .001, 1000
-def line_search(x, descent_direction_algorithm=steepest_descent_direction):
-    global last_BFGS_H, last_x, last_step_length
+TOLERANCE, MAX_STEP = .001, 10000
+def line_search(x, function_provider, descent_direction_algorithm=steepest_descent_direction):
+    global f_provider, last_BFGS_H, last_x, last_step_length
+    f_provider = function_provider
     last_BFGS_H = None
     step = 0
-    while norm_p(F_Provider.grad(*x)) >= TOLERANCE and step < MAX_STEP:
+    while norm_p(f_provider.grad(*x)) >= TOLERANCE and step < MAX_STEP:
         p = descent_direction_algorithm(x)
         alpha = backtracking_line_length(x, p)
         x = x + alpha * p
--- HW2/trust_region_algorithms.py
+++ HW2/trust_region_algorithms.py
@@ -1,14 +1,14 @@
 import numpy as np
-from HW2.tools import norm_p, RosenbrockProvider as F_Provider
+from HW2.tools import norm_p
 import matplotlib.pyplot as plt
 
 
 def approximate_model_generator(x, b_matrix):
-    return lambda p: F_Provider.f(*x) + np.dot(F_Provider.grad(*x), p) + 0.5 * np.dot(np.dot(p, b_matrix), p)
+    return lambda p: f_provider.f(*x) + np.dot(f_provider.grad(*x), p) + 0.5 * np.dot(np.dot(p, b_matrix), p)
 
 
 def cauchy_point(x, b_matrix, delta):
-    g_matrix = F_Provider.grad(*x)
+    g_matrix = f_provider.grad(*x)
     gT_b_g = np.dot(np.dot(g_matrix, b_matrix), g_matrix)
     gT_g = np.dot(g_matrix, g_matrix)
     g_norm = norm_p(g_matrix)
@@ -22,7 +22,7 @@ def cauchy_point(x, b_matrix, delta):
 
 
 def dog_leg(x, b_matrix, delta):
-    g_matrix = F_Provider.grad(*x)
+    g_matrix = f_provider.grad(*x)
     gT_b_g = np.dot(np.dot(g_matrix, b_matrix), g_matrix)
     gT_g = np.dot(g_matrix, g_matrix)
     grad_path_best_alpha = gT_g / gT_b_g
@@ -41,20 +41,23 @@ def dog_leg(x, b_matrix, delta):
             return p_b * delta / p_b_norm
 
 
-MAX_STEP, RHO_TOLERANCE, DELTA_HAT, ETA, EQUALITY_TOLERANCE = 1000, 1/.000001, 1.5, 1 / 5, .000001
-def trust_region(x, subproblem_solver=cauchy_point):
+MAX_STEP, RHO_TOLERANCE, DELTA_HAT, ETA, EQUALITY_TOLERANCE = 10000, 1/.000001, 1.5, 1 / 5, .000001
+def trust_region(x, function_provider, subproblem_solver=cauchy_point, plot=False):
+    global f_provider
+    f_provider = function_provider
+
     delta = DELTA_HAT/2
     step = 0
     rho = RHO_TOLERANCE
     initial_x = x
-    plot_x, plot_y = [], []
+    plot_y = []
     while step < MAX_STEP and abs(rho) <= RHO_TOLERANCE:
-        # b_matrix = np.zeros([2,2])
-        b_matrix = F_Provider.hessian(*x)  # use hessian matrix as Bk
+        # b_matrix = np.zeros([np.shape(x)[0], np.shape(x)[0]])
+        b_matrix = f_provider.hessian(*x)  # use hessian matrix as Bk
         p = subproblem_solver(x, b_matrix, delta)
         approximate_model = approximate_model_generator(x, b_matrix)
-        delta_f = F_Provider.f(*x) - F_Provider.f(*(x + p))
-        delta_m = approximate_model(np.zeros(2)) - approximate_model(p)
+        delta_f = f_provider.f(*x) - f_provider.f(*(x + p))
+        delta_m = approximate_model(np.zeros(np.shape(x)[0])) - approximate_model(p)
         rho = delta_f / delta_m
         if rho < .25:
             delta = .25 * delta
@@ -67,13 +70,13 @@ def trust_region(x, subproblem_solver=cauchy_point):
 
         step += 1
         # print('{}. new x: {}, rho={}'.format(step, x, rho))
-        plot_x.append(step)
         plot_y.append(rho)
 
-    plt.title('Rho on each step for point ({}, {})'.format(*initial_x))
-    plt.ylabel('Rho')
-    plt.xlabel('step')
-    plt.plot(plot_x, plot_y)
-    plt.show()
+    if plot:
+        plt.title('Rho on each step for point ({}, {})'.format(*initial_x))
+        plt.ylabel('Rho')
+        plt.xlabel('step')
+        plt.plot(plot_y)
+        plt.show()
     print('number of steps: ', step)
     return x
